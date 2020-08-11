b50213fa43f044492fc36b292917f1da97f65850
bhenk <bhenk@telfortglasvezel.nl>
2020-08-11 23:03:30 +0200

    list resources 3

--- application/gitzw/test/rawtest.php
+++ application/gitzw/test/rawtest.php
@@ -8,12 +8,12 @@ $a = ['a', 'b', 'c'];
 
 // $file = GZ::DATA.'/images//hnq/2020/_DSC0514_00019.jpg';
 
-// $exif = exif_read_data($file, 0, TRUE);
-// foreach ($exif as $key => $section) {
-// 	foreach ($section as $name => $val) {
-// 		echo "$key.$name: $val\n";
-// 	}
-// }
+$exif = exif_read_data($file, 0, TRUE);
+foreach ($exif as $key => $section) {
+	foreach ($section as $name => $val) {
+		echo "$key.$name: $val\n";
+	}
+}
    
 $b = array_slice($a, 2, 100);
 var_dump($b);
